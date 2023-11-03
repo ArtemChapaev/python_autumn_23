b90c45ced6556d5ff24a66c58a78049025f926f3
ArtemChapaev <ayuchapaev@gmail.com>
2023-11-03 17:33:06 +0300

    made hw(7)

---
+++ 07/fetcher.py
@@ -0,0 +1,96 @@
+import asyncio
+import sys
+import json
+from collections import Counter
+
+import aiohttp
+from bs4 import BeautifulSoup
+
+
+def check_arguments():
+    arguments = sys.argv
+    if len(arguments) != 3 and len(arguments) != 4:
+        raise RuntimeError(f"Need 2 or 3 arguments, it has got {len(arguments) - 1}")
+
+    if len(arguments) == 4:
+        if arguments[1] == '-c':
+            connections_count = arguments[2]
+            filename = arguments[3]
+        elif arguments[2] == '-c':
+            connections_count = arguments[3]
+            filename = arguments[1]
+        else:
+            raise RuntimeError("Need argument -c key")
+    else:
+        connections_count = arguments[1]
+        filename = arguments[2]
+
+    # if error type ValueError will be raised
+    connections_count = int(connections_count)
+
+    if connections_count < 1:
+        raise ValueError("connections_count must be bigger then 0")
+
+    if not isinstance(filename, str):
+        raise TypeError("arguments must be connections count and str (file with urls)")
+
+    return connections_count, filename
+
+
+class Fetcher:
+    def __init__(self, connections_count, count_top_words=3):
+        self.count_top_words = count_top_words
+        self.connections_count = connections_count
+
+        self.que = asyncio.Queue()
+
+    def _process_text(self, url, text):
+        filtered_text = BeautifulSoup(text, 'html.parser').get_text()
+
+        words = filtered_text.split()
+        most_common_words = Counter(words).most_common(self.count_top_words)
+
+        if not most_common_words:
+            return ""
+
+        return url + ": " + json.dumps([{word: count} for word, count in most_common_words])
+
+    async def _process_url(self, url):
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url) as resp:
+                text = await resp.text()
+
+                return await asyncio.get_event_loop().run_in_executor(
+                    None, self._process_text, url, text)
+
+    async def _create_process_worker(self):
+        while True:
+            url = await self.que.get()
+            try:
+                result = await self._process_url(url)
+                print(result)
+            finally:
+                self.que.task_done()
+
+    async def start(self, filename):
+        workers = [
+            asyncio.create_task(self._create_process_worker())
+            for _ in range(self.connections_count)
+        ]
+
+        # if not open FileNotFoundError will be raised
+        with open(filename, 'r', encoding='utf-8') as file:
+            for url in file:
+                await self.que.put(url)
+
+        await self.que.join()
+
+        for worker in workers:
+            worker.cancel()
+
+
+if __name__ == '__main__':
+    num_connections, url_filename = check_arguments()
+    fetcher = Fetcher(num_connections)
+
+    asyncio.run(fetcher.start(url_filename))
---
+++ 07/test_fetcher.py
@@ -0,0 +1,208 @@
+import unittest
+from unittest import mock
+import sys
+import asyncio
+
+from fetcher import Fetcher, check_arguments
+
+
+class TestFetcher(unittest.IsolatedAsyncioTestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    async def test_one_process_worker(self):
+        fetcher = Fetcher(1, 3)
+        with mock.patch("builtins.print") as mock_print:
+            with mock.patch.object(fetcher, '_process_url') as mock_process_url:
+                urls = ['url1', 'url2']
+                mock_process_url.side_effect = ['result1', 'result2']
+
+                worker = asyncio.create_task(fetcher._create_process_worker())
+
+                for url in urls:
+                    await fetcher.que.put(url)
+
+                await fetcher.que.join()
+
+                worker.cancel()
+
+        expected_calls = [
+            mock.call('url1'),
+            mock.call('url2')
+        ]
+        self.assertEqual(mock_process_url.mock_calls, expected_calls)
+
+        expected_calls = [
+            mock.call('result1'),
+            mock.call('result2')
+        ]
+        self.assertEqual(mock_print.mock_calls, expected_calls)
+
+    async def test_five_process_workers(self):
+        fetcher = Fetcher(2, 3)
+        with mock.patch("builtins.print") as mock_print:
+            with mock.patch.object(fetcher, '_process_url') as mock_process_url:
+                urls = ['url1', 'url2', 'url3', 'url4', 'url5']
+                mock_process_url.side_effect = ['result1', 'result2',
+                                                'result3', 'result4', 'result5']
+
+                workers = [
+                    asyncio.create_task(fetcher._create_process_worker())
+                    for _ in range(5)
+                ]
+
+                for url in urls:
+                    await fetcher.que.put(url)
+
+                await fetcher.que.join()
+
+                for worker in workers:
+                    worker.cancel()
+
+        expected_calls = [
+            mock.call('url1'),
+            mock.call('url2'),
+            mock.call('url3'),
+            mock.call('url4'),
+            mock.call('url5')
+        ]
+        self.assertEqual(mock_process_url.mock_calls, expected_calls)
+
+        expected_calls = [
+            mock.call('result1'),
+            mock.call('result2'),
+            mock.call('result3'),
+            mock.call('result4'),
+            mock.call('result5')
+        ]
+        self.assertEqual(mock_print.mock_calls, expected_calls)
+
+    async def test_five_process_workers_without_data(self):
+        fetcher = Fetcher(2, 3)
+        with mock.patch("builtins.print") as mock_print:
+            with mock.patch.object(fetcher, '_process_url') as mock_process_url:
+                # case without data
+                urls = []
+                mock_process_url.side_effect = []
+
+                workers = [
+                    asyncio.create_task(fetcher._create_process_worker())
+                    for _ in range(5)
+                ]
+
+                for url in urls:
+                    await fetcher.que.put(url)
+
+                await fetcher.que.join()
+
+                for worker in workers:
+                    worker.cancel()
+
+        mock_process_url.assert_not_called()
+        mock_print.assert_not_called()
+
+    def test_fetcher_process_text(self):
+        url = 'https://www.wikipedia.org'
+
+        with open('test_html_page.txt', 'r', encoding='utf-8') as file:
+            text = "".join(file.readlines())
+
+        fetcher = Fetcher(10, 3)
+        self.assertEqual(fetcher._process_text(url, text),
+                         'https://www.wikipedia.org: '
+                         '[{"your": 3}, {"Wikipedia": 2}, {"reading": 2}]'
+                         )
+
+        fetcher = Fetcher(10, 1)
+        self.assertEqual(fetcher._process_text(url, text),
+                         'https://www.wikipedia.org: [{"your": 3}]'
+                         )
+
+    def test_fetcher_process_text_without_data(self):
+        url = ''
+        text = ''
+
+        fetcher = Fetcher(10, 3)
+        self.assertEqual(fetcher._process_text(url, text),"")
+
+
+class TestFetcherArguments(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_fetcher_arguments_using(self):
+        sys.argv = ('fetcher.py', '-c', '10', 'urls.txt')
+        num_connections, url_filename = check_arguments()
+        self.assertEqual(num_connections, 10)
+        self.assertEqual(url_filename, 'urls.txt')
+
+        sys.argv = ('fetcher.py', 'urls.txt', '-c', '10')
+        num_connections, url_filename = check_arguments()
+        self.assertEqual(num_connections, 10)
+        self.assertEqual(url_filename, 'urls.txt')
+
+        sys.argv = ('fetcher.py', '1', 'urls')
+        num_connections, url_filename = check_arguments()
+        self.assertEqual(num_connections, 1)
+        self.assertEqual(url_filename, 'urls')
+
+    def test_fetcher_arguments_error_using(self):
+        with self.assertRaises(ValueError) as err:
+            sys.argv = ('fetcher.py', 'urls', '1')
+            check_arguments()
+        self.assertEqual(ValueError, type(err.exception))
+
+        with self.assertRaises(ValueError) as err:
+            sys.argv = ('fetcher.py', '-c', 'urls', '1')
+            check_arguments()
+        self.assertEqual(ValueError, type(err.exception))
+
+        with self.assertRaises(RuntimeError) as err:
+            sys.argv = ('fetcher.py', 'urls', '11', '1')
+            check_arguments()
+        self.assertEqual(RuntimeError, type(err.exception))
+
+        with self.assertRaises(ValueError) as err:
+            sys.argv = ('fetcher.py', '-c', '1')
+            check_arguments()
+        self.assertEqual(ValueError, type(err.exception))
+
+        with self.assertRaises(ValueError) as err:
+            sys.argv = ('fetcher.py', '1.5', 'urls')
+            check_arguments()
+        self.assertEqual(ValueError, type(err.exception))
+
+        with self.assertRaises(ValueError) as err:
+            sys.argv = ('fetcher.py', '-1', 'urls')
+            check_arguments()
+        self.assertEqual(ValueError, type(err.exception))
+
+        with self.assertRaises(ValueError) as err:
+            sys.argv = ('fetcher.py', '0', 'urls')
+            check_arguments()
+        self.assertEqual(ValueError, type(err.exception))
+
+        with self.assertRaises(ValueError) as err:
+            sys.argv = ('fetcher.py', '-c', '1.5', 'urls')
+            check_arguments()
+        self.assertEqual(ValueError, type(err.exception))
+
+        with self.assertRaises(ValueError) as err:
+            sys.argv = ('fetcher.py', '-c', '-1', 'urls')
+            check_arguments()
+        self.assertEqual(ValueError, type(err.exception))
+
+        with self.assertRaises(ValueError) as err:
+            sys.argv = ('fetcher.py', '-c', '0', 'urls')
+            check_arguments()
+        self.assertEqual(ValueError, type(err.exception))
+
+
+if __name__ == '__main__':
+    unittest.main()
